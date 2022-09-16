006ed47a156ddcf8e80d0167f8da2135d1d1ef0d
Greg Ross <greg.ross@networldsports.com>
2022-09-16 14:21:10 +0100

    - Added core data model and core data manager - Created three entity types, which will be used to save the user data

---
+++ Spenny/Models/CoreDataManager.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataManager.swift
+//  Spenny
+//
+//  Created by Greg Ross on 16/09/2022.
+//
+
+import Foundation
+import CoreData
+
+
+class CoreDataManager {
+    
+    let container: NSPersistentContainer
+    
+    init(){
+        self.container = NSPersistentContainer(name: "SpennyModel")
+        container.loadPersistentStores { description, error in
+            if let error = error{
+                print("\n Error loading Core Data Stores. Error: \(error.localizedDescription) \n")
+            }
+        }
+    }
+    
+}
